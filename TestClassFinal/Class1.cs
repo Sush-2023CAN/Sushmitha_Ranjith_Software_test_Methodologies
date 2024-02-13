915dcfe363fe2d33527f5a8ed75ed2249f0efa37
Sushmitha Ranjith <Sranjith5585@conestogac.on.ca>
2024-02-12 19:11:33 -0500

    Add project files.

---
+++ TestClassFinal/Class1.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework.Legacy;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Nunitdemonstration.Class1;
+
+namespace TestClassFinal
+{
+    public class Class1
+    {
+        [TestFixture]
+
+        public class TraingleTest1
+        {
+            [Test]
+
+            //Sushmitha Ranjith's UNIT TESTING
+
+            public void ValidTriangle()
+            {
+                //Arrange
+                int firstAngle = 60;
+                int secondAngle = 60;
+                int thirdAngle = 60;
+                String expected = "The triangle is valid.";
+
+                //Act
+                string actual = Triangle.ValidTriangle(firstAngle, secondAngle, thirdAngle);
+
+                //Assert
+                ClassicAssert.AreEqual(expected, actual);
+
+            }
+        }
+    }
+}
